b81bf472e8b69c751e83417be719c45d151f2d7e
David <dwd7290@yahoo.com>
2018-03-22 05:34:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function removeFromCart(item) {
   
   for(var i = 0; i < cart.length; i++) {
     console(item.toLowerCase());
-    console.log();
+    console.log(cart[i].itemName.toLowerCase());
     if (item.toLowerCase() === cart[i].itemName.toLowerCase()) {
       exist = [i];
     }
