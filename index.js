02d34536004f93c5dc00ae7d7d25c799b72ccf6e
David <dwd7290@yahoo.com>
2018-03-22 05:47:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function placeOrder(cardNumber) {
   if (cardNumber === '' || cardNumber === null || cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
-    
+    tmp = `Your total cost is ${total()}`;
   }
 }
 
