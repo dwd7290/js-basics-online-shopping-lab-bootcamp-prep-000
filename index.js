6d1a460f92eb97221ce260f6a94b567a0b965db1
David <dwd7290@yahoo.com>
2018-03-22 04:52:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,30 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price = Math.floor(Math.random() * 10) + 1;
+ var addedItem = {itemName: item, itemPrice: price};
+ cart.push(addedItem);
+ return `${item} has been added to your cart`;
 }
 
 function viewCart() {
   // write your code here
+  var list;
+  
+  if (cart.length === 0) {
+    return "Your shopping cart is empty.";
+  } 
+  
+  for (var i=0; i<cart.length; i++) {
+    if (i===0) {
+      list = `${cart[i].itemName} at $${cart[i].itemPrice}`;
+    } else if (i === cart.length - 1){
+      list += `, and ${cart[i].itemName} at $${cart[i].itemPrice}`;
+    } else {
+      list += `, ${cart[i].itemName} at $${cart[i].itemPrice}`;
+    }
+  }
+  return `In your cart, you have ${list}.`;
 }
 
 function total() {
@@ -28,3 +48,9 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
 }
+
+addToCart('Apple');
+addToCart('Orange');
+addToCart('Banana');
+addToCart('Strawberries');
+console.log(viewCart());
